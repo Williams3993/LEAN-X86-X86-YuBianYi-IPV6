9587d2ff7457e502a8750c1caab616bf11f943f4
Williams3993 <162794307+Williams3993@users.noreply.github.com>
2024-11-13 13:28:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,3 +7,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.77.1/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='OpenWrt-X86-X64-IPV4'/g' package/base-files/files/bin/config_generate
+
